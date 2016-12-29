4ff3ab7b302228ee20527ba64ee9752f315aa38e
Jake Craige <james.craige@gmail.com>
2016-12-28 16:07:57 -0800

    Update elixir file

--- test.ex
+++ test.ex
@@ -1,5 +1,6 @@
 if !true do
-  IO.puts "Hello, this line is realy long and should go over the credo line limit thing because it is really long and stuff"
+  # FIXME: This should be fixed
+  IO.puts("Hello, this line is really long and should go over the " + "credo line limit thing because it is really long and stuff")
 else
   # TODO: make this do something duh
   IO.puts "World"
